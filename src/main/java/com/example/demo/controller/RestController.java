c2b3f3be79bd853ae7b27edf6c868c7a61c54aa1
shbodake <sayalibodake28@gmail.com>
2021-12-17 00:13:11 +0530

    Added:Making REST Call to show Hello +firstname+lastname using put request method where firstname as path variable and lastname as parameter

--- src/main/java/com/example/demo/controller/RestController.java
+++ src/main/java/com/example/demo/controller/RestController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +18,7 @@ import com.example.demo.model.User;
  * @RequestParam:to pass the value as parameter.
  * @GetMapping:to get the name as a parameter.
  * @PostMpping:to post the first name and last name as parameter
+ * @PutMapping:call put method and pass firstname as path variable
  */
 
 public class RestController 
@@ -44,5 +46,11 @@ public class RestController
 	    @PostMapping("/post")
 	    public String sayHello(@RequestBody User user) {
 	        return "Hello " + user.getFirstName() + " " + user.getLastName() + " From Bridgelabz";
+	    }
+	    
+	    @PutMapping("/put/{firstName}")
+	    public String sayHello(@PathVariable String firstName,
+	                           @RequestParam(value = "lastName") String lastName) {
+	        return "Hello " + firstName + " " + lastName + " From bridgeLabz";
 	    }
 }
